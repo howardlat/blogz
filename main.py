73016de81100b65f3f973031ccbd72ab4180cabd
Latasha Howard <l_howard@outlook.com>
2018-10-08 19:15:16 -0500

    changes

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Task(db.Model):
+class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String)
 
     def __init__(self, name):
         self.name = name
@@ -23,24 +23,24 @@ class Task(db.Model):
 def index():
 
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
+        blog_name = request.form['blog']
+        new_blog = Blog(blog_name)
+        db.session.add(new_blog)
         db.session.commit()
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
+    blogs = Blog.query.filter_by(completed=False).all()
+    completed_blogs = Blog.query.filter_by(completed=True).all()
     return render_template('todos.html',title="Build A Blog", 
-        tasks=tasks, completed_tasks=completed_tasks)
+        blogs=blogs, completed_blogs=completed_blogs)
 
 
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
+@app.route('/delete-blog', methods=['POST'])
+def delete_blog():
 
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
+    blog_id = int(request.form['blog-id'])
+    blog = Blog.query.get(blog_id)
+    blog.completed = True
+    db.session.add(blog)
     db.session.commit()
 
     return redirect('/')
