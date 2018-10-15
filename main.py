a014eee33d53131de7e7c4ede0dc649a9d523b36
Latasha Howard <l_howard@outlook.com>
2018-10-14 21:06:47 -0500

    added id

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,28 +14,36 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(300))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, blog_id):
         self.title = title
         self.body = body
+        self.id = id
 
   
-@app.route('/blog')
+@app.route('/blog', methods=['POST', 'GET'])
 def blog():
-    return render_template('blog.html')
+    blog_form = request.form.get('id')
+    blog_args = request.args.get('id')
+    return render_template('blog.html', blog_form=blog_form, blog_args=blog_args)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
     return render_template('newpost.html')
 
-newposts = []
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     
     if request.method == 'POST':
-        newpost = request.form['newpost']    
-        newposts.append(newpost)
+        title_name = request.form['title']
+        body_name = request.form['body']
+        id_name = request.form['id']
+        newpost = Blog(title_name, body_name, id_name)
+        db.session.add(newpost)
+        db.session.commit()
+        
     
+    newposts = Blog.query.all()
     return render_template('blog.html',title="Build A Blog", newposts=newposts)
 
 
