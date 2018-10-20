188e8e1e52f7859208a64d1b220ea5fffe168e22
Latasha Howard <l_howard@outlook.com>
2018-10-19 22:04:51 -0500

    added user class

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:herman24@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:herman24@localhost:3306/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -13,11 +13,24 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(300))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
         
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
-               
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+        
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+                       
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
     blogs = Blog.query.all()
@@ -25,7 +38,9 @@ def blog():
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
-    
+
+    owner = User.query.filter_by(username=session['username']).first()
+        
     if request.method == 'POST':
 
         title = request.form['title']
@@ -33,11 +48,11 @@ def newpost():
         if len(title) < 1 or len(body) < 1:
             error = "Please enter a title or body"
             return render_template('newpost.html', error=error) 
-                         
+                    
         title = request.form['title']
         body = request.form['body']
         id = request.args.get('id')
-        newpost = Blog(title, body)
+        newpost = Blog(title, body, owner)
         db.session.add(newpost)
         db.session.commit()
         id = newpost.id
